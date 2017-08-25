a5f42efc436fd646b5afabc787a6de0098e2eb8c
Minh Doan <minh.doan04@gmail.com>
2017-08-25 00:52:00 -0400

    Use html input type file

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -7,41 +7,33 @@ var totalPeers = 0;
 
 var data = {};
 
-peer.on('open', function(id) {
+peer.on('open', function (id) {
     totalPeers++;
 });
 
-peer.on('connection', function(conn) {
-    conn.on('open', function() {
+peer.on('connection', function (conn) {
+    conn.on('open', function () {
         if (data.file) {
             conn.send(data);
         }
-    }); 
-});
-
-peer.on('error', function(err) {
-    console.log(err);
-});
-
-$(document).ready(function() {
-    var doNothing = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    }
-
-    var box = $('#box');
-    box.on('dragenter', doNothing);
-    box.on('dragover', doNothing);
-    box.on('drop', function(e) {
-        e.originalEvent.preventDefault();
-        var file = e.originalEvent.dataTransfer.files[0];
-        data.name = file.name;
-        data.file = file;
-        $('#link').text('http://localhost:3000/' + peer.id);
     });
 });
 
-window.onunload = window.onbeforeunload = function(e) {
+peer.on('error', function (err) {
+    console.log(err);
+});
+
+$(document).ready(function () {
+    $('#uploadFile').change(function (e) {
+        e.originalEvent.preventDefault();
+        var file = $('#uploadFile')[0].files[0];
+        data.name = file.name;
+        data.file = file;
+        $('#link').text('http://localhost:3000/' + peer.id);
+    })
+});
+
+window.onunload = window.onbeforeunload = function (e) {
     if (!!peer && !peer.destroyed) {
         peer.destroy();
     }
